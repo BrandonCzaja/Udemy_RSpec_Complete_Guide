4f1749f865048a80e0e783c6534e3eacfa979c7a
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-09-18 23:16:20 -0500

    Refactored the test so that I have two different examples instead of testing two things at the same time

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -12,20 +12,24 @@ require_relative '../lib/card.rb'
 # Step 1: Access RSpec and the describe method: RSpec.describe
 # Step 2: Identify what you want to test: Card
 # Step 3: Create a testing example group: do block
-# Step 4: Use the 'it' method to write the test output: it "has a type"
+# Step 4: Use the 'it' method to write the test output: it "has a rank && it has a suit"
 # Step 5: Create a 'do block' with a desription of the behvorior of the thing you are going to be building. - No tech, just behavior
 # Step 6: Logic comes in. Make your assumptions about what you are going to build and build it out: card = Card.new('Ace of Spades') 
 # Step 7: Use the 'expect()' and pass an argument that you want to evaluate: card.type to equal Ace of Spades
-RSpec.describe 'Card' do
+RSpec.describe Card do
     # Describe the expected behaivor of this thing you are about to build. 
     # Don't describe how it will be built, only what it does
 
-    it "has a type" do
+    # The card will now have a rank and a suit
+    it "has a rank" do
         # Make your first assumption - We assume we will have a card class, so we make an instance of it with the initial value of 'Ace of Spades'
-        card = Card.new('Ace of Spades')
-        # I expect card.type to equal Ace of Spades. eq is a Matcher
-        # I could also put equations here => expect(1 + 1).to eq(2)
-        expect(card.type).to eq('Ace of Spades') 
+        card = Card.new('Ace', 'Spades')
+        expect(card.rank).to eq('Ace')
+    end
+
+    it "has a suit" do
+        card = Card.new('Ace', 'Spades')
+        expect(card.suit).to eq('Spades')
     end
 end
 
