0fe5ab2f0b022d55e587795f6ec2f3c0a1f3930c
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-10-04 11:18:39 -0500

    Satisfy Matcher

---
+++ spec/Section_3_Matchers/satisfy_matcher_spec.rb
@@ -0,0 +1,22 @@
+# Allows you to create your own assertions and custom conditions
+
+
+# I am intentionally having this file fail to see the better way to have 'satisfy' return an error message
+RSpec.describe 'satisfy matcher' do
+    # subject{'racecar'} 
+    subject{'racecars'}
+
+    # Checking if a given string is a palindrome
+    it 'is a palindrome' do
+        # satisfy returns a boolean value 
+        # value in the satisfy block is the word 'racecar'
+        expect(subject).to satisfy {|value| value == value.reverse }
+    end
+
+    # You should always pass a string to satisfy - The errors will be clearer
+    it 'can accept a custom error message' do
+        expect(subject).to satisfy('be a palindrome') do |value|
+            value == value.reverse
+        end
+    end
+end
