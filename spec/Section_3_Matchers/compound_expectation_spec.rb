454fd790f5f810edea77432c92d363a71a147ad6
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-10-04 13:25:23 -0500

    Compound Expectations

---
+++ spec/Section_3_Matchers/compound_expectation_spec.rb
@@ -0,0 +1,31 @@
+# Combine more than one matcher on a single line
+
+RSpec.describe 25 do
+    it 'can test for multiple matchers' do
+        expect(subject).to be_odd
+        expect(subject).to be > 20
+    end
+
+    #  And keyword
+
+    it 'can test multiple matchers using the AND keyword' do
+        expect(subject).to be_odd and be > 20
+    end
+
+    it {is_expected.to be_odd and be > 20}
+end
+
+
+RSpec.describe 'caterpillar' do
+    it 'supports multiple matchers on a single line' do
+        expect(subject).to start_with('cat').and end_with('pillar')
+    end
+end
+
+
+RSpec.describe [:usa, :canada, :mexico] do
+    # Sample extracts one random value from the array
+    it 'can check for multiple possibilities' do
+        expect(subject.sample).to eq(:usa).or eq(:canada).or eq(:mexico)
+    end
+end
